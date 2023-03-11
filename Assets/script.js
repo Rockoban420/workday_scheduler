41a04e82ce7d731cfe6802c4fc8757b19745a2fa
Rockoban420 <lautaro.avellaneda@gmail.com>
2023-03-10 21:22:40 -0800

    final touches + readme edit

--- Assets/script.js
+++ Assets/script.js
@@ -1,80 +1,100 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(document).ready(function(){
-$(function () {
-  var headerEl = $('#currentDay');
-  var timeBlockDescription = JSON.parse(localStorage.getItem("timeBlockDescription")) ?? {};
-  var timeBlock = $('.time-block');
-  console.log(timeBlock);
+$(document).ready(function () {
+  $(function () {
+    var headerEl = $("#currentDay");
+    var timeBlockDescription =
+      JSON.parse(localStorage.getItem("timeBlockDescription")) ?? {};
+    var timeBlock = $(".time-block");
 
-  const setItems = function(){
-    for (const property in timeBlockDescription) {
-      let description = $('textarea#' + property);
-      description.val(timeBlockDescription[property]);
-    }
-  };
+    // setting the description to each time frame instantly
 
-  const getIDValue = function (string) {
-    const myArray = string.split ('');
-    let num = '';
-    let count = 1;
-    myArray.forEach(element => {
-      if (count > 5){
-        num += element;
+    const setItems = function () {
+      for (const property in timeBlockDescription) {
+        let description = $("textarea#" + property);
+        description.val(timeBlockDescription[property]);
       }
-      count++;
-    });
-    num = parseInt (num);
-    return num;
-  };
+    };
 
-  setItems();
-  timeBlock.on('click', '.saveBtn' , function(event){
+    // get the actual time number value on each div ID: eg turn hour-09 to 9
+
+    const getIDValue = function (string) {
+      const myArray = string.split("");
+      let num = "";
+      let count = 1;
+      myArray.forEach((element) => {
+        if (count > 5) {
+          num += element;
+        }
+        count++;
+      });
+      num = parseInt(num);
+      return num;
+    };
+
+    setItems();
+
+    // save button jquery event listener
+
+    timeBlock.on("click", ".saveBtn", function (event) {
       var currentID = event.delegateTarget.id;
-      var description = $('textarea#' + currentID);
+      var description = $("textarea#" + currentID);
       timeBlockDescription[currentID] = description.val();
-      localStorage.setItem ('timeBlockDescription' , JSON.stringify(timeBlockDescription));
-  });
+      localStorage.setItem(
+        "timeBlockDescription",
+        JSON.stringify(timeBlockDescription)
+      );
+    });
 
-  var currentHour = dayjs().hour();
-  for (let i = 0 ; i < timeBlock.length; i++){
-    let currentEl = $('#'+ timeBlock[i].id);
-    let currentElIDVal = getIDValue(timeBlock[i].id);
-    console.log (currentElIDVal);
-    if (currentElIDVal < currentHour){
-      currentEl.addClass ("past");
-    } else if (currentElIDVal === currentHour) {
-      currentEl.addClass ("present");
-    } else if (currentElIDVal > currentHour) {
-      currentEl.addClass ("future");
+    // setting the "state" of all time divs to either past
+    // present or future depending on the current time
+
+    var currentHour = dayjs().hour();
+    for (let i = 0; i < timeBlock.length; i++) {
+      let currentEl = $("#" + timeBlock[i].id);
+      // transforming id to int time value
+      let currentElIDVal = getIDValue(timeBlock[i].id);
+      // comparing it to the current time and assinging states
+      if (currentElIDVal < currentHour) {
+        currentEl.addClass("past");
+      } else if (currentElIDVal === currentHour) {
+        currentEl.addClass("present");
+      } else if (currentElIDVal > currentHour) {
+        currentEl.addClass("future");
+      }
     }
-  };
-  //
-  const dayOfWeek = function (number) {
-    if (number == 1) {
-      return 'Monday';
-    }
-    if (number == 2) {
-      return 'Tuesday';
-    }
-    if (number == 3) {
-      return 'Wednesday';
-    }
-    if (number == 4) {
-      return 'Thursday';
-    }
-    if (number == 5) {
-      return 'Friday';
-    }
-    if (number == 6) {
-      return 'Saturday';
-    }
-    if (number == 0) {
-      return 'Sunday';
-    }
-  }
-  headerEl.append (`Today is ${dayOfWeek(dayjs().day())} ${dayjs().month()+1}/${dayjs().date()}/${dayjs().year()}`)
-  // TODO: Add code to display the current date in the header of the page.
-});
+
+    // transforming dayjs day number to actual week days
+    // (need a plugin to work otherwise)
+
+    const dayOfWeek = function (number) {
+      if (number == 1) {
+        return "Monday";
+      }
+      if (number == 2) {
+        return "Tuesday";
+      }
+      if (number == 3) {
+        return "Wednesday";
+      }
+      if (number == 4) {
+        return "Thursday";
+      }
+      if (number == 5) {
+        return "Friday";
+      }
+      if (number == 6) {
+        return "Saturday";
+      }
+      if (number == 0) {
+        return "Sunday";
+      }
+    };
+
+    // adding the current day of week in words and date of the year in the header
+
+    headerEl.append(
+      `Today is ${dayOfWeek(dayjs().day())} ${
+        dayjs().month() + 1
+      }/${dayjs().date()}/${dayjs().year()}`
+    );
+  });
 });
--- README.md
+++ README.md
@@ -1,99 +1,10 @@
-# 05 Third-Party APIs: Work Day Scheduler
+## Description
+Welcome to my time limited quiz game! I made this quiz with the goal of making a functional game on a webpage and learned many things. The main thing is using local storage to store high scores at the end of the game and at the beggining if you click the view high scores button. Also learned very invaluable javascript lessons including button listeners and timer functions to time limit the game to only 60 seconds and adding 5 seconds when you input a right answer and taking 15 when you get a wrong one. The quiz is pretty simple its just a display of my newlearned knowledge of javascript! Hope you enjoy
 
-## Your Task
+Welcome to my Workday Scheduler! I made this scheduler site with the goal of being able to plan out your whole work day from 9-5 while being able to save specific text inputs for every hour. I learned how to implement APIs into my websites and learned how to use Day.js to fetch current date and time information to paint the time containers different colors and also display the current day and date on the header of the screen. While also using local storage to save every entry and it loading up on refresh. All you have to do is type in your task and click save and done! it will be there on reaload. Just a simple display of API inclusion before the big project next week. Thanks for reading!
 
-Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+## Screenshot
+![Screenshot](./Assets/workday_ss.png)
 
-You'll need to use the [Day.js](https://day.js.org/en/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
-
-## User Story
-
-```md
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
-
-## Acceptance Criteria
-
-```md
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
-
-The following animation demonstrates the application functionality:
-
-<!-- @TODO: create ticket to review/update image) -->
-![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/05-third-party-apis-homework-demo.gif)
-
-## Grading Requirements
-
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
-
-This Challenge is graded based on the following criteria:
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-  * Uses a date utility library to work with date and time
-
-### Deployment: 32%
-
-* Application deployed at live URL
-
-* Application loads with no errors
-
-* Application GitHub URL submitted
-
-* GitHub repo contains application code
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate
-
-* Application user interface style is clean and polished
-
-* Application resembles the mock-up functionality provided in the Challenge instructions
-
-### Repository Quality: 13%
-
-* Repository has a unique name
-
-* Repository follows best practices for file structure and naming conventions
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages
-
-* Repository contains quality README file with description, screenshot, and link to deployed application
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application
-
-* The URL of the GitHub repository, with a unique name and a README describing the project
-
-- - -
-© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+## Usage
+Just click on the link and take a look! https://rockoban420.github.io/workday_scheduler/
